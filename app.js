1f521302024fd3b4ea331056622cfcac42079666
renan bet rodrigues <renanbet@hotmail.com>
2020-04-27 22:16:42 -0300

    allow delete

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var app = express();
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-  // res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
   next();
 });
 
